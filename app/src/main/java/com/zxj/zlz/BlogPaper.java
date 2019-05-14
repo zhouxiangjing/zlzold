4a16acb4c545aff85f97867c39385af1a89bb661
zhouxiangjing <zhouxiangjing2018@163.com>
2019-05-14 15:02:21 +0800

    初始化首页

---
+++ app/src/main/java/com/zxj/zlz/BlogPaper.java
@@ -0,0 +1,27 @@
+package com.zxj.zlz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class BlogPaper extends AppCompatActivity {
+
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blog_paper);
+
+        Intent intent = getIntent();
+        Blog blog = (Blog) intent.getSerializableExtra("blog");
+
+
+        textView = findViewById(R.id.blog_content);
+
+        String data = blog.title + "\n\n" + blog.content;
+
+        textView.setText(data);
+
+    }
+}
