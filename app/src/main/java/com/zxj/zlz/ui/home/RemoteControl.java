b6633dbaa33797ef2e84957db35e3f7ec14e4287
zhouxiangjing <zhouxiangjing2018@163.com>
2019-08-02 15:01:07 +0800

    udp  ui

--- app/src/main/java/com/zxj/zlz/ui/home/RemoteControl.java
+++ app/src/main/java/com/zxj/zlz/ui/home/RemoteControl.java
@@ -8,6 +8,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.orhanobut.logger.AndroidLogAdapter;
@@ -27,9 +28,23 @@ public class RemoteControl extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remote_control);
 
-
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         new ScanfPITask().execute();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish(); // back button
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void onClickDeviceAddr(View view) {
